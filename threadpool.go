75ad3e13de15181cbb32a165a6aab4529cc20b5e
blademainer <blademainer@gmail.com>
2021-03-28 12:09:00 +0800

    init

--- threadpool.go
+++ threadpool.go
@@ -1,10 +1,15 @@
 package threadpool
 
-import "errors"
+import (
+	"context"
+	"errors"
+)
 
 var (
 	// ErrPoolSizeMustPresent pool size is illegal
 	ErrPoolSizeMustPresent = errors.New("pool size is illegal")
+	// ErrPoolIsFull pool is full
+	ErrPoolIsFull = errors.New("pool is full")
 )
 
 // ThreadPool thread pool interface
@@ -16,25 +21,46 @@ type ThreadPool interface {
 }
 
 type threadPool struct {
-	wc chan func()
+	wc  chan func()
+	ctx context.Context
 }
 
-func (t threadPool) Go(f func()) error {
-	panic("implement me")
+func (t *threadPool) Go(f func()) error {
+	select {
+	case t.wc <- f:
+		return nil
+	default:
+		return ErrPoolIsFull
+	}
 }
 
-func (t threadPool) Run(f func()) {
-	panic("implement me")
+func (t *threadPool) Run(f func()) {
+	t.wc <- f
+}
+
+func (t *threadPool) watch() {
+	for {
+		select {
+		case f := <-t.wc:
+			go func() {
+				f()
+			}()
+		case <-t.ctx.Done():
+			return
+		}
+	}
 }
 
 // NewPool create thread pool with size
-func NewPool(size int) (ThreadPool, error) {
+func NewPool(ctx context.Context, size int) (ThreadPool, error) {
 	if size <= 0 {
 		return nil, ErrPoolSizeMustPresent
 	}
 	wc := make(chan func(), size)
 	p := &threadPool{
-		wc: wc,
+		wc:  wc,
+		ctx: ctx,
 	}
+	go p.watch()
 	return p, nil
 }
